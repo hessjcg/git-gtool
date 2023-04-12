abc21a00c786ae1b526746b36c30dbe029c3f3cb
Jonathan Hess <hessjc@google.com>
2023-04-12 13:49:04 -0600

    feat: Add method to create a github client.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/google/go-github/v51 v51.0.0
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/viper v1.15.0
+	golang.org/x/oauth2 v0.6.0
 )
 
 require (
@@ -26,7 +27,6 @@ require (
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
---
+++ internal/model/client.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"context"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/google/go-github/v51/github"
+	"golang.org/x/oauth2"
+)
+
+// NewClient returns a new Github client that uses the same credentials
+// as the `gh` Github command line client.
+func NewClient(ctx context.Context, cwd string) (*github.Client, error) {
+	cmd := exec.Command("gh", "auth", "token")
+	cmd.Dir = cwd
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Unable to get github token using gh")
+	}
+	token := strings.Trim(string(output), "\n\r ")
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	return client, nil
+}
