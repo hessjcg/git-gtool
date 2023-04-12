f8eb5c2d38cb9e73a68552d01e3a5c1935ca4afa
Jonathan Hess <hessjc@google.com>
2023-04-12 11:15:16 -0600

    chore: remove unused code.

--- internal/gitrepo/git.go
+++ internal/gitrepo/git.go
@@ -7,11 +7,8 @@ import (
 	"os"
 	"path"
 	"regexp"
-	"strconv"
-	"strings"
 
 	git "github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/google/go-github/v51/github"
 	"github.com/hessjcg/git-gtool/internal/model"
@@ -45,165 +42,6 @@ func (r *GitRepo) GitExec(args ...string) (string, error) {
 	return Run(r.WorkDir, r.GitCommand, args...)
 }
 
-func (r *GitRepo) GetCommit(commitIsh string) (*object.Commit, error) {
-	hash, err := r.GitExec("rev-parse", commitIsh)
-	if err != nil {
-		return nil, err
-	}
-	return r.Repo.CommitObject(plumbing.NewHash(hash))
-}
-
-func (r *GitRepo) ApplyPatch(patch *object.Patch, stackName string) error {
-	patchFilename := path.Join(r.GitDir, "stack", "0000-rewrite.patch")
-	f, err := os.Create(patchFilename)
-	if err != nil {
-		return fmt.Errorf("can't write patch file, %v", err)
-	}
-	patch.Encode(f)
-	log.Print("Patching commit...")
-	out, err := r.GitExec("apply", "--index", "--3way", "--allow-empty", "-v", patchFilename)
-	log.Print(out)
-	if err != nil {
-		log.Print("Patch failed to apply. ")
-		log.Print("Manually apply patch hunks in **/*.rej files. Then use `git add` to")
-		log.Print("add the files to the index. Finally, run this command to continue: ")
-		log.Printf("  git stack rewrite --stack %s --continue", stackName)
-		return fmt.Errorf("patch did not apply clean, %v", err)
-	}
-	log.Print("Patch succeeded.")
-	return nil
-}
-
-func (r *GitRepo) NewBranch(branch string, hash plumbing.Hash) (*plumbing.Reference, error) {
-	n := plumbing.NewBranchReferenceName(branch)
-	ref := plumbing.NewHashReference(n, hash)
-
-	// The created reference is saved in the storage.
-	err := r.Repo.Storer.SetReference(ref)
-	if err != nil {
-		return nil, err
-	}
-
-	return ref, nil
-}
-
-func (r *GitRepo) ListCommits(target plumbing.Hash, base plumbing.Hash) (CommitRange, error) {
-	tc, err := r.Repo.CommitObject(target)
-	if err != nil {
-		return CommitRange{}, err
-	}
-
-	bc, err := r.Repo.CommitObject(base)
-	if err != nil {
-		return CommitRange{}, err
-	}
-	cr := CommitRange{
-		Target: tc,
-		Base:   bc,
-	}
-	cr.Commits, cr.Ancestor, cr.Linear, err = r.listCommits(target, base)
-	if err != nil {
-		return CommitRange{}, err
-	}
-	return cr, nil
-}
-
-func (r *GitRepo) listCommits(targetCommit plumbing.Hash, baseCommit plumbing.Hash) ([]*object.Commit, *object.Commit, bool, error) {
-
-	targetCommits, err := r.listCommitHistory(targetCommit, 100, baseCommit)
-	if err != nil {
-		return nil, nil, false, err
-	}
-
-	// if the last element in the targetCommits is the baseCommit, history is linear
-	// and we're done
-	if targetCommits[len(targetCommits)-1].Hash == baseCommit {
-		return targetCommits, targetCommits[len(targetCommits)-1], true, nil
-	}
-
-	baseCommits, err := r.listCommitHistory(baseCommit, 100, plumbing.ZeroHash)
-	if err != nil {
-		return nil, nil, false, err
-	}
-
-	var commits []*object.Commit
-	var commonParent *object.Commit
-
-	for i := 0; i < len(targetCommits) && commonParent == nil; i++ {
-		commits = append(commits, targetCommits[i])
-		for j := 0; j < len(baseCommit); j++ {
-			if targetCommits[i].Hash == baseCommits[j].Hash {
-				commonParent = targetCommits[i]
-				break
-			}
-		}
-	}
-
-	return commits, commonParent, false, nil
-}
-
-func (r *GitRepo) listCommitHistory(hash plumbing.Hash, n int, untilHash plumbing.Hash) ([]*object.Commit, error) {
-	baseCommit, err := r.Repo.CommitObject(hash)
-	if err != nil {
-		return nil, err
-	}
-
-	// get n commits starting with base and going back in time. if base was
-	// rebased, then there will be a few diverging commits between target and base
-	var baseCommits []*object.Commit
-	commit := baseCommit
-	for i := 0; i < n; i++ {
-		baseCommits = append(baseCommits, commit)
-		if untilHash != plumbing.ZeroHash && commit.Hash == untilHash {
-			break
-		}
-		if commit.NumParents() == 0 {
-			break
-		}
-		commit, err = commit.Parent(0)
-		if err != nil {
-			break
-		}
-	}
-	return baseCommits, nil
-}
-
-func (r *GitRepo) HeadBranch() (string, error) {
-	ref, err := r.Repo.Head()
-	if err != nil {
-		return "", err
-	}
-	return ref.Name().Short(), nil
-}
-
-func (r *GitRepo) Fetch() error {
-	out, err := r.GitExec("fetch", "origin")
-	if err != nil {
-		log.Print(out)
-		return fmt.Errorf("can't fetch from origin %v", err)
-	}
-	return nil
-}
-
-func (r *GitRepo) Lsremote() ([]*plumbing.Reference, error) {
-	out, err := r.GitExec("ls-remote", "-q")
-	if err != nil {
-		log.Print(out)
-		return nil, fmt.Errorf("can't fetch from origin %v", err)
-	}
-	lines := strings.Split(out, "\n")
-	refs := make([]*plumbing.Reference, len(lines))
-	for i, l := range lines {
-		f := strings.Split(l, "\t")
-		if len(f) != 2 {
-			return nil, fmt.Errorf("expected two fields for lsremote, got %v", l)
-		}
-		refs[i] = plumbing.NewHashReference(plumbing.ReferenceName(f[1]),
-			plumbing.NewHash(f[0]))
-	}
-	return refs, nil
-}
-
 func OpenGit(ctx context.Context, cwd string) (*GitRepo, error) {
 	gitcmd, err := GitExecutablePath(cwd)
 	if err != nil {
@@ -232,8 +70,6 @@ func OpenGit(ctx context.Context, cwd string) (*GitRepo, error) {
 		EnableDotGitCommonDir: true,
 	})
 
-	client, err := model.NewClient(ctx, workdir)
-
 	cfg, err := repo.Config()
 	if err != nil {
 		return nil, err
@@ -252,13 +88,15 @@ func OpenGit(ctx context.Context, cwd string) (*GitRepo, error) {
 		}
 	}
 
-	if name == "" || owner == "" {
-		return nil, fmt.Errorf("no remote on github.com found")
-	}
+	var c *github.Client
+	var r *github.Repository
+	if name != "" && owner == "" {
+		c, err = model.NewClient(ctx, workdir)
 
-	r, _, err := client.Repositories.Get(ctx, owner, name)
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving Github repo: %v", err)
+		r, _, err = c.Repositories.Get(ctx, owner, name)
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving Github repo: %v", err)
+		}
 	}
 
 	return &GitRepo{
@@ -266,7 +104,7 @@ func OpenGit(ctx context.Context, cwd string) (*GitRepo, error) {
 		WorkDir:    workdir,
 		GitDir:     gitdir,
 		Repo:       repo,
-		Client:     client,
+		Client:     c,
 		Owner:      owner,
 		Name:       name,
 		GithubRepo: r,
@@ -287,20 +125,4 @@ func GitExecutablePath(cwd string) (string, error) {
 		}
 	}
 	return gitexec, nil
-}
-
-// IsPullRequestRef returns the PR number parsed from the ref and true
-// when the ref starts with "refs/pull/"
-func IsPullRequestRef(ref *plumbing.Reference) (int, bool) {
-	const prefix = "refs/pull/"
-	if !strings.HasPrefix(ref.Name().String(), prefix) {
-		return 0, false
-	}
-	prNumStr := ref.Name().String()[len(prefix):]
-	prNumStr = prNumStr[0:strings.Index(prNumStr, "/")]
-	prNum, err := strconv.Atoi(prNumStr)
-	if err != nil {
-		return 0, false
-	}
-	return prNum, true
 }
